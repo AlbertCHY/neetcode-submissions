8ed1e53c5663277c8a317730ede3423564488351
AlbertCHY <49208412+AlbertCHY@users.noreply.github.com>
2026-06-28 19:05:06 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        reachable = 0
+        nextReachable = 0
+        steps = 0
+
+        if len(nums) <= 1:
+            return 0
+        
+        for i in range(len(nums) - 1):
+            nextReachable = max(nextReachable, i + nums[i])
+            if i == reachable:
+                steps += 1
+                reachable = nextReachable
+
+        return steps
