8d83cc3b35b5a48e4ccfa63efe6051e1639825ee
AlbertCHY <49208412+AlbertCHY@users.noreply.github.com>
2026-07-24 17:26:31 +0800

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split("/")
+        stack = []
+
+        for curr in path:
+            if curr == "..":
+                if stack:
+                    stack.pop()
+            elif curr != "." and curr != "":
+                stack.append(curr)
+
+        return "/" + "/".join(stack)
