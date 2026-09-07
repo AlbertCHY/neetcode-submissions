ab76101eff568f51f88b9f1f873358ab5ac1b0e0
AlbertCHY <49208412+AlbertCHY@users.noreply.github.com>
2026-09-07 14:04:45 +0800

    Add: accounts-merge - submission-0

---
+++ Data Structures & Algorithms/accounts-merge/submission-0.py
@@ -0,0 +1,47 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    
+    def union(self, x1, x2):
+        p1, p2 = self.find(x1), self.find(x2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        emailToAccount = {}
+
+        for i, account in enumerate(accounts):
+            for e in account[1:]:
+                if e in emailToAccount:
+                    uf.union(i, emailToAccount[e])
+                else:
+                    emailToAccount[e] = i
+
+        emailGroup = defaultdict(list)
+        for e, i in emailToAccount.items():
+            leader = uf.find(i)
+            emailGroup[leader].append(e)
+
+        result = []
+        for i, emails in emailGroup.items():
+            name = accounts[i][0]
+            result.append([name] + sorted(emailGroup[i]))
+        return result
+
+        
